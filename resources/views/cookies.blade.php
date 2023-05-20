452254376a3d89568ea96c81f36823f167b3581b
RaffiDevYT <raffidevyt@gmail.com>
2023-05-20 07:14:58 -0700

    chore(resources\views\cookies.blade.php) added cookies page
    chore(resources\views\cookies.blade.php) added cookies page

--- resources/views/cookies.blade.php
+++ resources/views/cookies.blade.php
@@ -6,19 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookies Notice</title>
 
-    <!--  Bootstrap css file  -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-
     <!--  font awesome icons  -->
     <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
 	
     <!--  custom css file  -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/cookies.css') }}">
 
     <!-- s -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" />
 
     <!--  Responsive css file  -->
@@ -116,9 +110,100 @@
     <!-- Cookies Area END -->
 
     <!-- Hero Area -->
-    <h1>SOON</h1>
+    <div class="kocak flex_align_justify">
+      <div class="terms_service">
+          <div class="tc_item tc_head flex_align_justify">
+            <div class="icon flex_align_justify">
+              <ion-icon name="terminal-outline"></ion-icon>
+            </div>
+            <div class="text">
+              <h2>Cookie Notice</h2>
+              <p>Last updated on September 12 2022</p>
+            </div>
+          </div>
+          <div class="tc_item tc_body">
+            <ol>
+              <li>
+                <h3>What are cookies?</h3>
+                <p>Cookies are small files which are stored on your device by a website, unique to your web browser. The web browser will send these files to the website each time it communicates with the website.
+                   Cookies are used by this website for a variety of reasons which are outlined below.</p>
+              </li>
+              <li>
+                <h3>Necessary cookies</h3>
+                <p>Necessary cookies are required for this website to function. These are used by the website to maintain your session, allowing for you to submit any forms, log into the website amongst other essential behaviour. It is not possible to disable these within the website, however you can disable cookies altogether via your browser.</p>
+              </li>
+              <li>
+                <h3>Functional cookies</h3>
+                <p>Functional cookies allow for the website to work as you choose. For example, enabling the "Remember Me" option as you log in will create a functional cookie to automatically log you in on future visits.</p>
+              </li>
+              <li>
+                <h3>Analytical cookies</h3>
+                <p>Analytical cookies allow both this website, and any third party services used by this website, to collect non-personally identifiable data about the user. This allows us (the website staff) to continue to improve the user experience and understand how the website is used.</p>
+                </br>
+                <p>Further information about cookies can be found online, including the ICO's website which contains useful links to further documentation about configuring your browser.</p>
+              </li>
+              <li>
+                <h3>Configuring cookie use</h3>
+                <p>By default, only necessary cookies are used by this website. However, some website functionality may be unavailable until the use of cookies has been opted into.
+                   You can opt into, or continue to disallow, the use of cookies using the cookie notice popup on this website. If you would like to update your preference, the cookie notice popup can be re-enabled by clicking the button below.</p>
+              </li>
+            </ol>
+          </div>
+          <div class="tc_item tc_foot flex_align">
+              <button class="accept_btn">SHOW COOKIE POPUP</button>
+          </div>
+      </div>
+    </div>
     <!-- Hero Area END -->
 
+        <!-- ======================= Footer ======================= -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col">
+                        <h4>company</h4>
+                        <ul>
+                            <li><a href="#about"></i> about</a>
+                            <li><a href="blog/"></i> blog</a>
+                            <li><a href="{{ route('login.action') }}""></i> Login</a>
+                            <li><a href="{{ route('register.action') }}""></i> Register</a>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>get help</h4>
+                        <ul>
+                            <li><a href="https://discord.gg/qqbXQJ9dak" target="_blank">Discord</a></li>
+                            <li><a href="https://alquran.raffidev.com" target="_blank">Alquran Digital</a></li>
+                            <li><a href="#">payment options</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>contact info</h4>
+                        <ul>
+                            <li><a href="https://wa.me/6285157868689" target="_blank"> <i class="fas fa-phone"></i> +62 851-5786-8689</a></li>
+                            <li><a href="mailto:raffi@raffidev.com"> <i class="fas fa-envelope"></i> raffi@raffidev.com</a></li>
+                            <li><a href="#"> <i class="fas fa-map-marker-alt"></i>  Curug, Tangerang, Banten</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>follow me</h4>
+                        <div class="social-links">
+                            <a href="https://wa.me/6285157868689" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                            <a href="https://"><i class="fab fa-discord"></i></a>
+                            <a href="https://instagram.com/raffidev" target="_blank"><i class="fab fa-instagram"></i></a>
+                            <a href="https://github.com/RaffiDevYT" target="_blank"><i class="fab fa-github git"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div class="lastline">
+                    <p class="para" style="color: #fff;">
+                        © 2020
+                        <a href="https://instagram.com/raffidev" target="_blank" style="text-decoration: none;"><span style="color: #fff;">Raffi Solutions</span></a>
+                    </p>
+        </div>
+    <!-- ======================= End Footer ======================= -->
 
     <!-- JAVA SCRIPT -->
     <script src="https://unpkg.com/scrollreveal"></script>
@@ -127,7 +212,5 @@
     <script src="{{ asset('js/particles.min.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js"></script>
 </body>
 </html>
