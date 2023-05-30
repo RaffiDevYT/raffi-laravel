eb0450c35a497d68ed9f0e81f615e277b962394b
RaffiDevYT <raffidevyt@gmail.com>
2023-05-30 00:24:36 -0700

    change theme
    by raffidev

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -206,7 +206,7 @@
     <meta name="description" content="in making..">
     <!--    <meta name="theme-color" content="#5d040e"> for navigator color on mobile -->
     <meta name="author" content="RafiAthallah">
-    <meta property="og:title" content="RaionStore Minecraft">
+    <meta property="og:title" content="Rafi Website">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://raffidev.com">
     <meta property="og:image" content="./images/logo.webp">
@@ -218,60 +218,17 @@
 
     <title></Building></title>
 </head>
-<!-- Dixept: https://discord.gg/VKfC3TAQv3 -->
 <body>
     <div id="particles-js"></div>
     <main>
-        <img src="{{ asset('img/Logo Baru.png') }}" alt="Logo of ServerName" draggable="false"/>
         <h1>ON WORKING</h1>
         <p>We are building a website blog please wait</p>
         <hr>
         <div>
-            <a href="https://instagram.com/raffidev" target="_blank">
-            <svg height="36" width="36" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-	        viewBox="0 0 551.034 551.034" xml:space="preserve">
-            <g id="XMLID_13_">
-
-		    <linearGradient id="XMLID_2_" gradientUnits="userSpaceOnUse" x1="275.517" y1="4.5714" x2="275.517" y2="549.7202" gradientTransform="matrix(1 0 0 -1 0 554)">
-		    <stop  offset="0" style="stop-color:#E09B3D"/>
-		    <stop  offset="0.3" style="stop-color:#C74C4D"/>
-		    <stop  offset="0.6" style="stop-color:#C21975"/>
-		    <stop  offset="1" style="stop-color:#7024C4"/>
-	        </linearGradient>
-	        <path id="XMLID_17_" style="fill:url(#XMLID_2_);" d="M386.878,0H164.156C73.64,0,0,73.64,0,164.156v222.722
-		    c0,90.516,73.64,164.156,164.156,164.156h222.722c90.516,0,164.156-73.64,164.156-164.156V164.156
-		    C551.033,73.64,477.393,0,386.878,0z M495.6,386.878c0,60.045-48.677,108.722-108.722,108.722H164.156
-		    c-60.045,0-108.722-48.677-108.722-108.722V164.156c0-60.046,48.677-108.722,108.722-108.722h222.722
-		    c60.045,0,108.722,48.676,108.722,108.722L495.6,386.878L495.6,386.878z"/>
-	
-		    <linearGradient id="XMLID_3_" gradientUnits="userSpaceOnUse" x1="275.517" y1="4.5714" x2="275.517" y2="549.7202" gradientTransform="matrix(1 0 0 -1 0 554)">
-		    <stop  offset="0" style="stop-color:#E09B3D"/>
-		    <stop  offset="0.3" style="stop-color:#C74C4D"/>
-		    <stop  offset="0.6" style="stop-color:#C21975"/>
-		    <stop  offset="1" style="stop-color:#7024C4"/>
-            </linearGradient>
-	        <path id="XMLID_81_" style="fill:url(#XMLID_3_);" d="M275.517,133C196.933,133,133,196.933,133,275.516
-		    s63.933,142.517,142.517,142.517S418.034,354.1,418.034,275.516S354.101,133,275.517,133z M275.517,362.6
-		    c-48.095,0-87.083-38.988-87.083-87.083s38.989-87.083,87.083-87.083c48.095,0,87.083,38.988,87.083,87.083
-		    C362.6,323.611,323.611,362.6,275.517,362.6z"/>
-	
-		    <linearGradient id="XMLID_4_" gradientUnits="userSpaceOnUse" x1="418.306" y1="4.5714" x2="418.306" y2="549.7202" gradientTransform="matrix(1 0 0 -1 0 554)">
-		    <stop  offset="0" style="stop-color:#E09B3D"/>
-		    <stop  offset="0.3" style="stop-color:#C74C4D"/>
-		    <stop  offset="0.6" style="stop-color:#C21975"/>
-		    <stop  offset="1" style="stop-color:#7024C4"/>
-	        </linearGradient>
-	        <circle id="XMLID_83_" style="fill:url(#XMLID_4_);" cx="418.306" cy="134.072" r="34.149"/>
-            </g>
-</svg>
-            </a>
             <a href="https://discord.gg/VKfC3TAQv3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-discord" viewBox="0 0 16 16">
                     <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"/>
                 </svg>
-            </a>
-            <a href="https://wa.me/6285157868689">
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/></svg>
             </a>
         </div>
     </main>
