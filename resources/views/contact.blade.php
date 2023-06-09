550c46cb94fcde2f6dcb0ee34c4a8930357c4eb2
RaffiDevYT <raffidevyt@gmail.com>
2023-06-09 07:03:06 -0700

    added contact information
    by raffidev

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords" content="Rafi Athallah">
+    <link rel="shortcut icon" href="{{ asset('img/data/rafi.jpg') }}">
+    <meta name="description" content="Hello, my name is Rafi Athallah I created this website for my personal data and photos">
+    <meta name="author" content="RafiAthallah">
+    <meta property="og:site_name" content="Rafi Athallah">
+    <meta property="og:url" content="https://raffidev.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Rafi Athallah">
+    <meta property="og:description" content="Hello, my name is Rafi Athallah I created this website for my personal data and photos">
+
+    <!-- Title -->
+    <title>Rafi Athallah</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+</head>
+<body>
+<div class="page contact" id="page-contact">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="content section-padding">
+                            <div oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false' style='-moz-user-select: none; cursor: default;' class="section-head">
+                                <h5><span>C</span>ontact Me</h5>
+                                <h3>Contact</h3>
+                            </div>
+
+                            <div class="contact-info">
+                                <div oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false' style='-moz-user-select: none; cursor: default;' class="sub-head mb-50">
+                                    <h5>Contact <span> Info</span></h5>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="item">
+                                            <span class="icon"><i class="fas fa-phone"></i></span>
+                                            <div class="cont">
+                                                <h6>Phone</h6>
+                                                <p><a href="tel:+6288214141947">+6288214141947</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="item">
+                                            <span class="icon"><i class="fa-brands fa-whatsapp"></i></span>
+                                            <div class="cont">
+                                                <h6>Whatsapp</h6>
+                                                <p><a href="https://web.whatsapp.com/send?phone=6288214141947">+6288214141947</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="item">
+                                            <span class="icon"><i class="fas fa-envelope"></i></span>
+                                            <div class="cont">
+                                                <h6>Email</h6>
+                                                <p><a href="mail:torafi.athallah@raffidev.com">rafi.athallah@raffidev.com</span></a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="item">
+                                            <span class="icon"><i class="fas fa-envelope"></i></span>
+                                            <div class="cont">
+                                                <h6>Email</h6>
+                                                <p><a href="mail:toraffidevyt@gmail.com">raffidevyt@gmail.com</span></a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="item">
+                                            <span class="icon"><i class="fas fa-map-marker-alt"></i></span>
+                                            <div class="cont">
+                                                <h6>Alamat </h6>
+                                                <p>Jl.Nusa Indah 3 Blok d23 no 24</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="item">
+                                            <span class="icon"><i class="fa-solid fa-briefcase"></i></span>
+                                            <div class="cont">
+                                                <h6>Perkerjaan</h6>
+                                                <p>Developers & <a href="https://discord.gg/VSrsVCt9u8">ISCI - TEAM [MODERATOR]</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 simg bg-img" data-background="img/5.jpg"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
